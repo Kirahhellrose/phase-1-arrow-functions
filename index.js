8d5a5f0fa8e272ad481b16a1d6be2fc0a491ecbd
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-10 14:47:37 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => 2000 / 100;
+
+//Arrow Function Call Square
+const square = x => x * x;
+square(2)
+
+//Arrow Function Called Add
+const add= (a,b) => a + b;
+add(3,4)
